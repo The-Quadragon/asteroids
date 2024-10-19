436ec10339351cfd56d0a9c825bd59d6259dee57
The-Quadragon <crsmith1991@gmail.com>
2024-10-19 08:55:32 -0600

    add player draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -11,13 +12,16 @@ def main():
     frame = pygame.time.Clock()
     dt = 0
     
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
-        pygame.display.flip()
+        player.draw(screen)
         
+        pygame.display.flip()
         dt += frame.tick(60)/1000
         
 if __name__ == "__main__":
