8ba33a5fa136043c0e53589840fa8c0851fb9b47
The-Quadragon <crsmith1991@gmail.com>
2024-10-19 09:59:08 -0600

    create update groups

--- main.py
+++ main.py
@@ -7,22 +7,32 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
     dt = 0
     
+
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
         screen.fill("black")
-        player.draw(screen)
         
+        for item in updateable:
+            item.update(dt)
+        
+        for item in drawable:
+            item.draw(screen) 
+
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000
         
 if __name__ == "__main__":
